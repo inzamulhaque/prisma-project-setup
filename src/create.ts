d5bf262ee6365635d2a2aaabc9b2bd108dce2d0a
inzamulhaque <mdihalif@gmail.com>
2024-03-11 03:01:41 +0600

    insert data in mamy to many relation database

--- src/create.ts
+++ src/create.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -11,68 +11,77 @@ const create = async () => {
   //       content: "test 2",
   //     },
   //   });
-
   //   console.log(result);
+  // const createMany = await prisma.post.createMany({
+  //   data: [
+  //     {
+  //       title: "test 3",
+  //       authorName: "test 3",
+  //       content: "test 3",
+  //     },
+  //     {
+  //       title: "test 4",
+  //       authorName: "test 4",
+  //       content: "test 4",
+  //     },
+  //     {
+  //       title: "test 5",
+  //       authorName: "test 5",
+  //       content: "test 5",
+  //     },
+  //   ],
+  // });
+  // console.log(createMany);
 
-  const createMany = await prisma.post.createMany({
-    data: [
-      {
-        title: "test 3",
-        authorName: "test 3",
-        content: "test 3",
-      },
+  // const createUser = await prisma.user.create({
+  //   data: {
+  //     username: "User2",
+  //     email: "user2@user.com",
+  //     role: UserRole.user,
+  //   },
+  // });
 
-      {
-        title: "test 4",
-        authorName: "test 4",
-        content: "test 4",
-      },
+  // console.log(createUser);
 
-      {
-        title: "test 5",
-        authorName: "test 5",
-        content: "test 5",
-      },
+  // const createProfile = await prisma.profile.create({
+  //   data: {
+  //     bio: "This is my bio",
+  //     userId: 3,
+  //   },
+  // });
 
-      {
-        title: "test 3",
-        authorName: "test 3",
-        content: "test 3",
-      },
+  // console.log(createProfile);
 
-      {
-        title: "test 4",
-        authorName: "test 4",
-        content: "test 4",
-      },
+  // const createCategory = await prisma.category.create({
+  //   data: {
+  //     name: "javascript",
+  //   },
+  // });
 
-      {
-        title: "test 5",
-        authorName: "test 5",
-        content: "test 5",
-      },
+  // console.log(createCategory);
 
-      {
-        title: "test 3",
-        authorName: "test 3",
-        content: "test 3",
+  const createPost = await prisma.post.create({
+    data: {
+      title: "Title 1",
+      content: "Content 1",
+      authorId: 3,
+      postCategory: {
+        create: {
+          // categoryId: 1,
+          category: {
+            connect: {
+              id: 1,
+            },
+          },
+        },
       },
-
-      {
-        title: "test 4",
-        authorName: "test 4",
-        content: "test 4",
-      },
-
-      {
-        title: "test 5",
-        authorName: "test 5",
-        content: "test 5",
-      },
-    ],
+    },
+    include: {
+      postCategory: true,
+    },
   });
 
-  console.log(createMany);
+  console.log(createPost);
 };
 
 export default create;
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const main = async () => {
   // update();
   // deleteData();
   // upsert();
-  paginationSoring();
+  // paginationSoring();
 };
 
 main();
